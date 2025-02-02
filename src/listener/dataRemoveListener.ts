e28fb0d2d43a00e61b2c3ec3246607c5680ef079
Matt Taylor <matt@matthewjosephtaylor.com>
2025-02-02 12:13:55 -0600

    remove dep on IDBKeyRange object

--- src/listener/dataRemoveListener.ts
+++ src/listener/dataRemoveListener.ts
@@ -4,15 +4,12 @@ import { getDbStoreData } from "../data-store/getDbStoreData";
 import { idbValidKeyToString } from "../data-store/idbValidKeyToString";
 import { writeDbStoreData } from "../data-store/writeDbStoreData";
 
-
 export const dataRemoveListener: ConnectionListener<
-  DataConnectionMap, "data.remove"
+  DataConnectionMap,
+  "data.remove"
 > = async (props) => {
   const { dbStore, query } = props.detail.body;
   const dataMap = await getDbStoreData(dbStore);
-  if (query instanceof IDBKeyRange) {
-    throw new Error("IDBKeyRange not supported");
-  }
   const stringKey = await idbValidKeyToString(query);
   delete dataMap[stringKey];
   await writeDbStoreData(dbStore, dataMap);
