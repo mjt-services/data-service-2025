7e8fd25ecc69c2e0609d8983ea2836734bdd7da3
Matt Taylor <matt@matthewjosephtaylor.com>
2025-02-18 13:01:38 -0600

    empty same as undefined for query

--- src/object-store/query/dataAndQueryToProjection.ts
+++ src/object-store/query/dataAndQueryToProjection.ts
@@ -1,9 +1,8 @@
-import { isUndefined } from "@mjt-engine/object";
+import { isEmpty, isUndefined } from "@mjt-engine/object";
 import { search } from "jmespath";
 
-
 export const dataAndQueryToProjection = (data: unknown, query?: string) => {
-  if (isUndefined(query)) {
+  if (isEmpty(query) || isUndefined(query)) {
     return data;
   }
   return search(data, query);
