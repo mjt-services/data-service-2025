082a61e77682ad473fec05cc6bf883ab59650677
Matt Taylor <matt@matthewjosephtaylor.com>
2025-02-23 11:48:48 -0600

    publish update event on data.remove operation

--- src/listener/dataRemoveListener.ts
+++ src/listener/dataRemoveListener.ts
@@ -1,12 +1,11 @@
 import type { ConnectionListener } from "@mjt-engine/message";
 import { isDefined } from "@mjt-engine/object";
-import {
-  type DataConnectionMap,
-} from "@mjt-services/data-common-2025";
+import { type DataConnectionMap } from "@mjt-services/data-common-2025";
 import { getObjectStoreData } from "../object-store/file/getObjectStoreData";
 import { writeObjectStoreData } from "../object-store/file/writeObjectStoreData";
 import { queryToKeys } from "../object-store/query/queryToKeys";
 import { queryToObjectStores } from "../object-store/query/queryToObjectStores";
+import { publishUpdateEvent } from "../event/publishUpdateEvent";
 
 export const dataRemoveListener: ConnectionListener<
   DataConnectionMap,
@@ -18,12 +17,20 @@ export const dataRemoveListener: ConnectionListener<
   for (const store of stores) {
     const dataMap = await getObjectStoreData(store);
     const keys = await queryToKeys(query);
+    const deletedValues: unknown[] = [];
     for (const key of keys) {
+      const valueMaybe = dataMap[key];
+      if (isDefined(valueMaybe)) {
+        deletedValues.push(valueMaybe);
+      }
       delete dataMap[key];
     }
+
     await writeObjectStoreData(store, dataMap);
+
+    for (const value of deletedValues) {
+      await publishUpdateEvent(value);
+    }
   }
   return { success: true };
 };
-
-
