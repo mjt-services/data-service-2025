34d56abcd001e560b67ec1b6830002f86fb5c74a
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-03 13:35:13 -0600

    added Entity type

--- src/event/publishUpdateEvent.ts
+++ src/event/publishUpdateEvent.ts
@@ -1,6 +1,8 @@
 import {
   isChildObject,
+  type ChildUpdateSubject,
   type DATA_EVENT_MAP,
+  type ObjectUpdateSubject,
   type UpdateSubject,
 } from "@mjt-services/data-common-2025";
 import { getConnection } from "../_connection";
@@ -9,10 +11,18 @@ export const publishUpdateEvent = async (value: unknown) => {
   if (!isChildObject(value)) {
     return;
   }
-  const { parentId } = value;
+  const { parentId, id } = value;
   const con = await getConnection();
   con.publish<UpdateSubject, typeof DATA_EVENT_MAP>({
     payload: undefined,
     subject: `update.${parentId}`,
+  });
+  con.publish<ChildUpdateSubject, typeof DATA_EVENT_MAP>({
+    payload: undefined,
+    subject: `child_update.${parentId}`,
+  });
+  con.publish<ObjectUpdateSubject, typeof DATA_EVENT_MAP>({
+    payload: undefined,
+    subject: `object_update.${value}`,
   });
 };
