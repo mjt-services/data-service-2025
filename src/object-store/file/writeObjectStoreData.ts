f78fc285107870cc36e442a0776edf16c9a7cefd
Matt Taylor <matt@matthewjosephtaylor.com>
2025-03-03 14:16:13 -0600

    temp files for writes to ensure consistent reads

--- src/object-store/file/writeObjectStoreData.ts
+++ src/object-store/file/writeObjectStoreData.ts
@@ -3,12 +3,16 @@ import type { ObjectStore } from "@mjt-services/data-common-2025";
 import * as fs from "fs/promises";
 import type { DataMap } from "../type/DataMap";
 import { objectStoreToFilePath } from "./objectStoreToFilePath";
+import { dirname, join } from "path";
 
-export const writeObjectStoreData = (
+export const writeObjectStoreData = async (
   objectStore: ObjectStore,
   data: DataMap
 ) => {
   const filePath = objectStoreToFilePath(objectStore);
+  const tempFilePath = join(dirname(filePath), `${filePath}.tmp`);
   const bytes = Bytes.toMsgPack(data);
-  return fs.writeFile(filePath, bytes, { flag: "w" });
+
+  await fs.writeFile(tempFilePath, bytes, { flag: "w" });
+  await fs.rename(tempFilePath, filePath);
 };
